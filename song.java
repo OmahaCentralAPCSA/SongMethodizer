d2312c3326c71b1983021e288a88543d4c4f7abc
Collin Holmquist <collinholmquist@gmail.com>
2020-08-18 13:09:39 -0500

    Create song.java

---
+++ song.java
@@ -0,0 +1,20 @@
+public class Main {
+
+  public static void main(String [] args) {
+    //main will only call the playSong() method
+    playSong();
+  }
+  
+  public static void playSong() {
+    //this will be the method that will 'play' the song
+  
+  }
+  
+  //create whatever other static methods you need below
+
+
+
+
+
+
+}
